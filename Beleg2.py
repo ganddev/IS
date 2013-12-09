6cf6a1054514d2b6704ff8120da91146ccd1b284
DanielPinkpank <daniel.pinkpank@gmail.com>
2013-12-09 12:59:36 +0100

    Yeehaa

--- Beleg2.py
+++ Beleg2.py
@@ -4,15 +4,20 @@ import numpy as np
 import matplotlib .pyplot as plt
 import pylab
 from sqlalchemy.sql.functions import coalesce
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
 
 
 def createTrainData(tettas, size):
-    dataX1 = np.arange(0.0, 1.0, 1.0/size).reshape(size,1)
-    dataX2 = np.arange(0.0, 1.0, 1.0/size).reshape(size,1)
-    gauss = np.random.normal(0,0.1,size).reshape(size,1)
-    dataY = (tettas[0]+tettas[1]*dataX1+tettas[2]*dataX2)+gauss
+    """Erzeugt die Trainingsdaten"""
+    dataX1 = np.random.normal(0, 1, size).reshape(size,1)
+    dataX2 = np.random.normal(0, 1, size).reshape(size,1)
+    gauss = np.random.normal(0, 0.5, size).reshape(size,1)
+    dataY = (tettas[0] + tettas[1] * dataX1 + tettas[2] * dataX2) + gauss
     value1 = np.ones(size).reshape(size,1)
-    features = np.concatenate((np.concatenate((value1,dataX1),axis=1),dataX2),axis=1)
+    features = np.concatenate((np.concatenate((value1, dataX1),axis=1), dataX2),axis=1)
+
     return features, dataY
 
 def scale(data):
@@ -25,14 +30,14 @@ def scale(data):
 
 def hypothesis(tettas, featureVektor):
     """Die multivariate lineare Regression"""
-    return np.dot(tettas, featureVektor)
+    return np.dot(tettas.T, featureVektor)
 
-def costs(tettas, xMatrix, yVektor):
+def costs(tettas, features, dataY):
     """Kostenfunktion"""
     summe = 0.0
-    factor = 1.0 / (2 * len(yVektor))
-    for i in range(len(xMatrix)):
-        summe += np.sum(np.square(hypothesis(tettas.T, np.array([xMatrix[0][i],xMatrix[1][i],xMatrix[2][i]])) - yVektor))
+    factor = 1.0 / (2 * len(dataY))
+    for i in range(len(dataY)):
+        summe += np.sum(np.square(hypothesis(tettas, features.T[i]) - dataY))
     return factor * summe
 
 def logisticFunction(tettas, xVektor):
@@ -40,20 +45,37 @@ def logisticFunction(tettas, xVektor):
     return 1.0/ (1+np.exp(-hypothesis(tettas,xVektor)))
 
 
-def gradiantDesc(tettas, features, dataY, tettaJOld, dataCosts, learnrate):
+def gradiantDesc(tettas, features, dataY, learnrate):
     """Gradienten-Abstiegs-Verfahren"""
-    factor = 1/len(dataY)
+    factor = 1.0/len(dataY)
     summe = 0.0
+    dataCostsOld = costs(tettas,features.T,dataY)
     for i in range(len(features)):
-        summe += np.dot(np.square(hypothesis(tettas.T, np.array([features[0][i],features[1][i],features[2][i]])) - dataY[i])[0],np.array([features[0][i],features[1][i],features[2][i]]))
-    print summe
-    tettaJTmp = learnrate*factor*summe
-    #return tettaJTmp
+        summe += np.dot(hypothesis(tettas, features[i]) - dataY[i][0],features[i])
+    tettasTmp = tettas - (learnrate * factor * summe)
+    dataCostsNew = costs(tettasTmp,features.T,dataY)
+    print tettas
+    print dataCostsNew
+    if (dataCostsOld - dataCostsNew > 0.1):
+        return gradiantDesc(tettasTmp, features, dataY, learnrate)
+    else:
+        return tettasTmp, dataCostsNew
 
 if __name__ == "__main__":
-    tettas = np.array([1,2,3])
-    #tettas1 = np.array([[1,2,3],[3,4,3],[5,6,3],[5,6,3]]).T
-    #print tettas1
-    [features,dataY] = createTrainData(tettas,10)
-    gradiantDesc(tettas, features.T, dataY, 0, [], 0.5)
+    [features, dataY] = createTrainData(np.array([0,1,1]),100)
+    [tettas, costs] = gradiantDesc(np.array([2,1,4]), features, dataY, 0.5)
+
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+    ax.scatter(features[:,2], features[:,1], dataY[:,0], s=20, c='r', label='DataPoints')
+
+    ax.legend()
+
+    x = np.linspace(-2,2,40)
+    y = x
+    x, y = np.meshgrid(x,y)
+    z = tettas[0] + tettas[1] * x + tettas[2] * y
+    ax.plot_surface(x,y,z)
+
+    plt.show()
 
