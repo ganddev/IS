26d4a2312cbf36a3bb236f1622fa00bb055251e6
DanielPinkpank <daniel.pinkpank@gmail.com>
2013-12-16 19:40:20 +0100

    Logistic

--- Beleg2.py
+++ Beleg2.py
@@ -69,7 +69,8 @@ def logisticRegression(tettas, features):
     return z, featureSum
 
 if __name__ == "__main__":
-    [features, dataY] = createTrainData(np.array([0,1,1]),1000)
+    [features, dataY] = createTrainData(np.array([0,1,1]),100)
+
     [tettas, dataCosts] = gradiantDesc(np.array([2,1,4]), features, dataY, 0.1, [])
     [tettas2, dataCosts2] = gradiantDesc(np.array([2,1,4]), features, dataY, 0.2, [])
     [tettas3, dataCosts3] = gradiantDesc(np.array([2,1,4]), features, dataY, 0.4, [])
@@ -77,23 +78,18 @@ if __name__ == "__main__":
     [tettas5, dataCosts5] = gradiantDesc(np.array([2,1,4]), features, dataY, 0.8, [])
     [tettas6, dataCosts6] = gradiantDesc(np.array([2,1,4]), features, dataY, 1.0, [])
 
-
-    logisticRegression(tettas, features)
-
     plt.title("3D Data")
     fig1 = plt.figure(1)
-    ax = fig1.gca(projection='3d')
-    ax.scatter(features[:,2], features[:,1], dataY[:,0], s=20, c='r', label='DataPoints')
-
-    x = np.linspace(-2,2,100)
-    y = x
-    x, y = np.meshgrid(x,y)
-    z = tettas[0] + tettas[1] * x + tettas[2] * y
-    ax.plot_surface(x,y,z)
-    ax.legend()
+    ax1 = fig1.gca(projection='3d')
+    ax1.scatter(features[:,2], features[:,1], dataY[:,0], s=20, c='r', label='DataPoints')
+    x1 = np.linspace(-2,2,100)
+    y1 = x1
+    x1, y1 = np.meshgrid(x1,y1)
+    z1 = tettas[0] + tettas[1] * x1 + tettas[2] * y1
+    ax1.plot_surface(x1,y1,z1)
+    ax1.legend()
 
     fig2 = plt.figure(2)
-    plt.subplot(212)
     plt.title("Costs")
     plt.plot(np.arange(len(dataCosts)),dataCosts, label = "Learnrate 0.1")
     plt.plot(np.arange(len(dataCosts2)),dataCosts2, label = "Learnrate 0.2")
@@ -102,28 +98,77 @@ if __name__ == "__main__":
     plt.plot(np.arange(len(dataCosts5)),dataCosts5, label = "Learnrate 0.8")
     plt.plot(np.arange(len(dataCosts6)),dataCosts6, label = "Learnrate 1.0")
     plt.legend(bbox_to_anchor=(1.0, 1), loc=1, borderaxespad=0.)
+    plt.xlabel('Iterations')
     plt.ylabel('Costs')
 
 
-    [y,featureSum] = (logisticRegression(tettas,features))
-    featureSum.sort()
     fig3 = plt.figure(3)
-    ax = fig3.add_subplot(222)
+
+
     plt.subplot(222)
-    ax.scatter(*zip(*featureSum))
-    ax.set_xlim(-10, 10)
-    ax.set_ylim(-0.1, 1.1)
     plt.title("Logistic")
+    x2 = np.linspace(-15,15,100) # 100 linearly spaced numbers
+    y2 = 1.0/ (1+np.exp(-x2))
+    plt.plot(x2,y2)
+
+    #[z, featureSumme] = logisticRegression(tettas,features)
+    ##z1 = tettas[0] + z * x + z*y
+    #fig3 = plt.figure()
+    #ax3 = fig3.gca(projection='3d')
+    #ax3.scatter(features[:,2], features[:,1], z, s=20, c='b', label='DataPoints')
+    ##bx.plot_surface(x, y ,z1)
+    #ax3.legend()
 
 
-    [z, featureSumme] = logisticRegression(tettas,features)
-    #z1 = tettas[0] + z * x + z*y
-    fig3 = plt.figure()
-    ax = fig3.gca(projection='3d')
-    ax.scatter(features[:,2], features[:,1], z, s=20, c='b', label='DataPoints')
-    #bx.plot_surface(x, y ,z1)
-    ax.legend()
+    plt.subplot(223)
+    # class 0:
+    # covariance matrix and mean
+    cov0 = np.array([[5,-4],[-4,3]])
+    mean0 = np.array([2.,3])
+    # number of data points
+    m0 = 100
+    # generate m0 gaussian distributed data points with
+    # mean0 and cov0.
+    r0 = np.random.multivariate_normal(mean0, cov0, m0)
+
+    # covariance matrix
+    cov1 = np.array([[5,-3],[-3,3]])
+    mean1 = np.array([1.,1])
+    m1 = 100
+    r1 = np.random.multivariate_normal(mean1, cov1, m1)
+
+    x31 = r1.T[0].reshape(m0,1)
+    #x32 = scale(r1.T[0])
+    #x3 = np.union1d(x31,x32).reshape(2*m0,1)
+    #x3.append(r1.T[0].reshape(m0,1))
+    y31 = r1.T[1].reshape(m0,1)
+    #y32 = scale(r1.T[1])
+    #y3 = np.union1d(y31,y32).reshape(2*m0,1)
+    #y3.append(r1.T[1].reshape(m0,1))
+
+    value2 = np.ones(m0).reshape(m0,1)
+    #print x3
+    x3 = np.concatenate((value2, x31),axis=1)
+
+    [tettas_log, dataCosts_log] = gradiantDesc(np.array([10,-8]), x3, y31, 0.01, [])
+    print dataCosts_log
+    plt.scatter(r0[...][...,0], r0[...,1], c='b', marker='o')
+    plt.scatter(r1[...,0], r1[...,1], c='r', marker='x')
+
+    x4 = np.linspace(-7,7,100) # 100 linearly spaced numbers
+    y4 = tettas_log[0] + tettas_log[1] * x4
+    plt.plot(x4,y4)
+
+    plt.subplot(224)
+    plt.title("Costs")
+    plt.plot(np.arange(len(dataCosts_log)),dataCosts_log, label = "Learnrate 0.1")
+
+    print x3
+    [y,featureSum] = (logisticRegression(tettas_log,x3))
+    featureSum.sort()
+    print featureSum
+    ax2 = fig3.add_subplot(221)
+    ax2.scatter(*zip(*featureSum))
+    ax2.set_ylim(-0.1, 1.1)
 
     plt.show()
-
- #erste werte gehoert zu der aus den features; jeweilige  feature Vektoren summieren entsprechend sortieren... -6+3*x1+3*x2
